400a06f97dc6c5087e0b06dab959e25aba3d4b89
Healer928802 <78316893+Healer928802@users.noreply.github.com>
2024-12-02 10:48:19 +0200

    Initial Commit

---
+++ CatPhotoApp/CatPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatPhotoAppApp.swift
+//  CatPhotoApp
+//
+//  Created by Anton on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
